70aa1b2ccb20652e0265ac268c44dba5dea11ae1
DannielNavas <dannielnavas@gmail.com>
2023-11-09 05:51:54 -0500

    ✨ Add icons and labels to bottom tab navigation.

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,5 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { Image } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
 import Account from '../screens/Account';
 import Favorite from '../screens/Favorite';
 import Pokedex from '../screens/Pokedex';
@@ -9,10 +11,41 @@ const Tab = createBottomTabNavigator();
 const Navigation = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Favorite" component={Favorite} />
-      <Tab.Screen name="Pokedex" component={Pokedex} />
-      <Tab.Screen name="Account" component={Account} />
+      <Tab.Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color, size }) => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Mi cuenta",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+}
+
+const renderPokeball = () => {
+  return (
+    <Image source={require('../assets/pokeball.png')} style={{width: 75, height: 75, top: -15}} />
   );
 }
 
