304cbf9af9e048bb423f2850eedaf15a94c92cee
DannielNavas <dannielnavas@gmail.com>
2023-11-16 06:17:38 -0500

    ✨ Add form validation to login form and install Formik and Yup

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,13 +1,40 @@
+import { useFormik } from 'formik';
 import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import * as Yup from 'yup';
 
 const LoginForm = () => {
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
     <View>
       <Text style={styles.title}>Iniciar sesión</Text>
-      <TextInput placeholder='Nombre de usuario' style={styles.input} autoCapitalize='none' />
-      <TextInput placeholder='Contraseña' style={styles.input} autoCapitalize='none' secureTextEntry={true} />
-      <Button title='Iniciar sesión' onPress={() => console.log('entrando')} />
+      <TextInput
+        placeholder="Nombre de usuario"
+        style={styles.input}
+        autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
+      />
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+      />
+      <Button title="Iniciar sesión" onPress={formik.handleSubmit} />
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   );
 }
@@ -26,7 +53,26 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10
+  },
+  error: {
+    color: '#f00',
+    textAlign: 'center',
+    marginTop: 20,
   }
 })
+
+const initialValues = () => {
+  return {
+    username: '',
+    password: ''
+  }
+}
+
+const validationSchema = () => {
+  return {
+    username: Yup.string().required('El nombre de usuario es obligatorio'),
+    password: Yup.string().required('La contraseña es obligatoria')
+  }
+}
 
 export default LoginForm;
