c5952f08cf0d6317b7fbc43ccd321e9763a8aedf
DannielNavas <dannielnavas@gmail.com>
2023-11-16 06:03:46 -0500

    ✨ Add login form with input fields and button

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const LoginForm = () => {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput placeholder='Nombre de usuario' style={styles.input} autoCapitalize='none' />
+      <TextInput placeholder='Contraseña' style={styles.input} autoCapitalize='none' secureTextEntry={true} />
+      <Button title='Iniciar sesión' onPress={() => console.log('entrando')} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 50,
+    marginBottom: 15
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
+  }
+})
 
 export default LoginForm;
